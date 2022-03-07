543e5919176f55fc051a8fc58f94e28382218d20
theyvzr <theyvzr@gmail.com>
2022-03-07 23:39:47 +0300

    all comm

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,17 @@
-from flask import Flask
-import boto3
-from config import config
-
-client = boto3.client()
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/hello/<name>", methods=["Get", "POST"])
+def hello_world(name):
+   # name = request.args.get("key")
+    if request.method == "GET":
+        return "Hello World"
+    elif request.method == "POST":
+        return f"Hello, {name}"
+
+@app.route("/bye")
+def goodbye_world():
+    return "Good bye"
+
+app.run(debug=True)
