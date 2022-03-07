e7e3103986f2d734b9d3d2330ab1592318374cda
theyvzr <theyvzr@gmail.com>
2022-03-08 02:27:37 +0300

    boto3 start and list

--- boto3_app.py
+++ boto3_app.py
@@ -1,2 +1,27 @@
 import boto3
+from config import config
 
+client = boto3.client('ec2',
+    aws_access_key_id=config["access_key"],
+    aws_secret_access_key=config["secret_access_key"],
+    region=config["region"]
+)
+
+response = client.describe_instances(
+    Fliters=[
+        {
+            'Name': 'key-name',
+            'Values': [
+                'BestCloud4Me'
+            ]
+        }
+    ]
+)
+
+InstancesIds = []
+for instance in response["Reservations"][0]["Instances"]:
+    InstancesIds.append(instance["InstanceId"])
+
+response = client.start_instances(
+    InstancesIds=InstancesIds
+)
