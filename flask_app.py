06e343ffdae3c283b0f077e7937958de1db15046
theyvzr <theyvzr@gmail.com>
2022-03-08 01:03:38 +0300

    flask_app2

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
-@app.route("/hello/<name>", methods=["Get", "POST"])
-def hello_world(name):
-   # name = request.args.get("key")
+#@app.route("/hello/<name>", methods=["GET", "POST"])
+#def hello_world_url_param(name):
+#    surname = request.args.get("surname")
+#    if surname == None:
+#        surname = "Soyad Girilmedi!!!"
+#    if request.method == "GET":
+#        return "Hello World"
+#    elif request.method == "POST":
+#        return f"ad: {name}, soyad: {surname}!"
+
+@app.route("/hello/<name>", methods=["GET", "POST"])
+def hello_world_url_param(name):
+    surname = request.args.get("surname")
+    if surname == None:
+        surname = "Soyad Girilmedi!!!"
     if request.method == "GET":
         return "Hello World"
     elif request.method == "POST":
-        return f"Hello, {name}"
+        return make_response(jsonify(FirstName=name, LastName=surname), 500)
 
-@app.route("/bye")
-def goodbye_world():
-    return "Good bye"
+@app.route("/json", methods=["POST"])
+def parse_json():
+    json_data = request.get_json()
+    print(json_data["key"])
+    print(type(json_data))
+    return ""
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
