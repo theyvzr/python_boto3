e771c47fe784039033e814a3d5f862a3bacc1c72
theyvzr <theyvzr@gmail.com>
2022-03-09 18:34:54 +0300

    boto3 update

--- boto3_app.py
+++ boto3_app.py
@@ -2,7 +2,7 @@ import boto3
 from config import config
 
 client = boto3.client('ec2',
-    aws_access_key_id=config["access_key"],
+aws_access_key_id=config["access_key"],
     aws_secret_access_key=config["secret_access_key"],
     region_name=config["region"]
 )
@@ -12,13 +12,19 @@ response = client.describe_instances(
         {
             'Name': 'key-name',
             'Values': [
-                'BestCloud4Me',
+                'BestCloud4Me'
             ]
-        },
+        }
     ]
 )
 
-print(response)
+print(response["Reservations"][0])
+
+#print(response["Reservations"][0]["Instances"][0])
+
+#for instance in response["Reservations"][0]["Instances"]:
+#    print(instance["InstanceId"])
+
 #response = client.start_instances(
 #    InstancesIds=InstancesIds
 #)
