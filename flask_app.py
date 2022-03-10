9099581ea87f7a37862ec2ee6a73ac486ce227a9
theyvzr <theyvzr@gmail.com>
2022-03-10 03:48:30 +0300

    update all file

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, make_response
+import boto3
 from config import config
 import logging
 
@@ -9,33 +10,67 @@ logging.basicConfig(
     level=config['log_level']
 )
 
-#@app.route("/hello/<name>", methods=["GET", "POST"])
-#def hello_world_url_param(name):
-#    surname = request.args.get("surname")
-#    if surname == None:
-#        surname = "Soyad Girilmedi!!!"
-#    if request.method == "GET":
-#        return "Hello World"
-#    elif request.method == "POST":
-#        return f"ad: {name}, soyad: {surname}!"
+@app.route("/ec2/list", methods=["GET"])
+def function_list():
 
-@app.route("/hello/<name>", methods=["GET", "POST"])
-def hello_world_url_param(name):
-    surname = request.args.get("surname")
-    if surname == None:
-        surname = "Soyad Girilmedi!!!"
-        #logging.info("dfjnsdfkfnekfsfvk") loga root mesaj gönderir
-    if request.method == "GET":
-        return "Hello World"
-    elif request.method == "POST":
-        return make_response(jsonify(FirstName=name, LastName=surname), 500)
+    access_key = request.args.get("access_key")
+    secret_access_key = request.args.get("secret_access_key")
+    region = request.args.get("region")
 
-@app.route("/json", methods=["POST"])
-def parse_json():
-    json_data = request.get_json()
-    print(json_data["key"])
-    print(type(json_data))
-    return ""
+    client = boto3.client('ec2',
+        aws_access_key_id=f"{access_key}",
+        aws_secret_access_key=f"{secret_access_key}",
+        region_name=f"{region}"
+    )
+
+
+    response = client.describe_instances()
+
+    try:
+        request.method == "GET"
+        InstanceIds = []
+        for instance in response["Reservations"][0]["Instances"]:
+            InstanceIds.append(instance["InstanceId"])
+        return jsonify(InstanceIds=InstanceIds), 200
+    except:
+        return "Wrong HTTP methods used!"
+
+@app.route("/ec2/start", methods=["POST"])
+def function_start():
+
+    access_key = request.args.get("access_key")
+    secret_access_key = request.args.get("secret_access_key")
+    region = request.args.get("region")
+    InstanceId = request.args.get("instanceid")
+
+    client = boto3.client('ec2',
+        aws_access_key_id=f"{access_key}",
+        aws_secret_access_key=f"{secret_access_key}",
+        region_name=f"{region}"
+        )
+
+    if request.method == "POST":
+        response = client.start_instances(InstanceIds=[InstanceId])
+        return "Your instance has been started.", 200
+
+
+@app.route("/ec2/stop", methods=["COPY"])
+def function_stop():
+
+    access_key = request.args.get("access_key")
+    secret_access_key = request.args.get("secret_access_key")
+    region = request.args.get("region")
+    InstanceId = request.args.get("instanceid")
+
+    client = boto3.client('ec2',
+        aws_access_key_id=f"{access_key}",
+        aws_secret_access_key=f"{secret_access_key}",
+        region_name=f"{region}"
+        )
+
+    if request.method == "COPY":    
+        response = client.stop_instances(InstanceIds=[InstanceId])
+        return "Your instance has been stopped."
 
 if __name__ == "__main__":
     app.run(host=config["host"], port=config["port"], debug=True)
